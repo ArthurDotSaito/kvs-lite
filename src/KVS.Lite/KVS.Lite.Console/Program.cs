a7bf8fdd5354199777e487877f1d0484a12cfb76
Arthur Saito <arthur_saito1998@hotmail.com>
2024-08-09 22:18:49 -0300

    very basic ttl

--- src/KVS.Lite/KVS.Lite.Console/Program.cs
+++ src/KVS.Lite/KVS.Lite.Console/Program.cs
@@ -6,6 +6,9 @@ Console.WriteLine("Starting KVS Store");
 
 var kvStore = new KeyValueStore();
 
-kvStore.Set("Test Key", "Test Value");
+kvStore.Set("Test Key", "Test Value", 5);
 
+Console.WriteLine(kvStore.Get("Test Key"));
+
+Thread.Sleep(6000);
 Console.WriteLine(kvStore.Get("Test Key"));
